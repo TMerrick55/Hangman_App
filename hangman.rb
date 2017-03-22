f336f38c74b7449ca6df3a8613746800c186292a
TMerrick55 <tmerrick44@gmail.com>
2017-03-22 18:40:09 -0400

    Hangman Web App Ver. 0.80

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,35 @@
 class Hangman
 
-	
+	attr_accessor :word, :correct_letters, :wrong_count
+
+	def initialize(word)
+		@word = word.upcase
+		@wrong_count = 0
+		@correct_letters = container_array
+	end
+
+	def container_array
+		container = Array.new(word.length, '_')
+	end
+
+	def guess_letter(letter)
+		letter = letter.upcase
+		if word.include?(letter)
+			word.each_char.with_index do |val, pos|
+				if val == letter
+					@correct_letters[pos] = val
+				end
+			end
+		else
+			@wrong_count += 1
+		end
+	end
+
+	def valid_letter_input?
+		if word.match(/[^a-zA-Z]/)
+			false
+		else
+			true
+		end
+	end
+end
--- test_hangman.rb
+++ test_hangman.rb
@@ -3,4 +3,66 @@ require_relative 'hangman.rb'
 
 class TestHangman < Minitest::Test
 
-	
+	def test_container_is_array
+		hangman_object = Hangman.new('Salutations')
+		results = hangman_object.correct_letters
+		assert_equal(Array, results.class)	
+	end
+
+	def test_array_length_11
+		hangman_object = Hangman.new('Salutations')
+		results = hangman_object.correct_letters
+		assert_equal(11, results.length)	
+	end
+
+	def test_array_elements
+		hangman_object = Hangman.new('Salutations')
+		results = hangman_object.correct_letters
+		assert_equal(['_','_','_','_','_','_','_','_','_','_','_'], results)	
+	end
+
+	def test_array_correct_guess_A
+		hangman_object = Hangman.new('Salutations')
+		hangman_object.guess_letter('A')
+		results = hangman_object.correct_letters
+		assert_equal(['_','A','_','_','_','A','_','_','_','_','_'], results)	
+	end
+
+	def test_array_incorrect_guess_C_wrong_count_1
+		hangman_object = Hangman.new('Salutations')
+		hangman_object.guess_letter('C')
+		results = hangman_object.wrong_count
+		assert_equal(1, results)
+	end
+
+	def test_array_correct_guess_beckon
+		hangman_object = Hangman.new('Beckon')
+		hangman_object.guess_letter('b')
+		results = hangman_object.correct_letters
+		assert_equal(['B','_','_','_','_','_'], results)
+	end
+
+	def test_invalid_word_input_false
+		hangman_object = Hangman.new('Friend1')
+		results = hangman_object.valid_letter_input?
+		assert_equal(false, results)
+	end
+
+	def test_valid_word_input_true
+		hangman_object = Hangman.new('SaLutAtiOns')
+		results = hangman_object.valid_letter_input?
+		assert_equal(true, results)
+	end
+
+	def test_invalid_symbols_false
+		hangman_object = Hangman.new('Salutations!')
+		results = hangman_object.valid_letter_input?
+		assert_equal(false, results)
+	end
+
+	def test_invalid_space_false
+		hangman_object = Hangman.new('Twenty Four')
+		results = hangman_object.valid_letter_input?
+		assert_equal(false, results)
+	end
+end
